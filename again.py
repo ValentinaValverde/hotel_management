f0e7282161852bf82e65b9934d74cbc51949d8af
Valentina Valverde <vvgreenville@gmail.com>
2023-10-15 11:18:40 -0400

    Still trying to make it so that if a room_num already exists in floor_num, the user can only check out. The next step after that would be to check if the names are the same (to check out).

--- again.py
+++ again.py
@@ -1,41 +1,65 @@
 hotel = {
-    '1': {
-        '185': ['George Jefferson', 'Wheezy Jefferson'],
+    1: {
+        185: ['George Jefferson', 'Wheezy Jefferson'],
     },
-    '2': {
-        '237': ['Jack Torrance', 'Wendy Torrance'],
+    2: {
+        237: ['Jack Torrance', 'Wendy Torrance'],
     },
-    '3': {
-        '333': ['Neo', 'Trinity', 'Morpheus']
+    3: {
+        333: ['Neo', 'Trinity', 'Morpheus']
     }
 }
 
+while len(hotel) > 0 and len(hotel) < 10:
 
-user_name = input("please type your name: ")
-floor_num = input("please type your floor number: ")
-room_num = input("please type your room number: ")
-checking = input("are you checking in or out? please type 'in' for check-in and 'out' for check-out. -> ")
+    user_name = input("please type your name: ")
+    floor_num = int(input("please type your floor number: "))
+    room_num = int(input("please type your room number: "))
+    checking = input("are you checking in or out? please type 'in' for check-in and 'out' for check-out. -> ")
 
 
-floor = {
-    floor_num: {
-        room_num: [user_name]
+    new_entry = {
+        floor_num: {
+            room_num: [user_name]
+        }
     }
-}
 
 
-def in_or_out():
-    if checking == "in":
-        hotel.update(floor)
+    def in_or_out():
+        if checking == "in":
+            hotel.update(new_entry)
+            print_hotel_list()
+
+        elif checking == "out":
+            del hotel[floor_num]
+            print_hotel_list()
+        
+        else:
+            print(" ")
+            print("please type in or out.")
+            print(" ")
+
+
+    def print_hotel_list():
+        print(" ")
+        print("hotel list:")
         print(hotel)
-    elif checking == "out":
-        del hotel[floor_num]
-        print(hotel)
+        print(" ")
 
 
-def print_hotel_list():
+    if floor_num > 5:
+        print("sorry, that floor level does not exist. goodbye!")
+        break
+
+    #i want to make it so that if it exists in the list, they can ONLY check out
+    for x in hotel: #note that x stands for floor_num
+        for y in x:  #note that y stands for room_num
+            if y in x:
+                print("mama mia.")
+
+
+    in_or_out()
+    print("new entry: ", new_entry)
     print(" ")
-    print("hotel list:")
-    print(" ")
-    print(hotel)
+
 
